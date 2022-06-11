98a6a12561bc589cce6befce50f686e3fe56f1e0
gorgeusgeorge <ilikepython.really@gmail.com>
2022-06-11 15:51:43 +0300

    added tests for parse function

--- main.py
+++ main.py
@@ -19,7 +19,16 @@ if __name__ == '__main__':
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-
+    assert parse('http://example.com/?country=usa&city=detroit&district=downtown') == {'country': 'usa', 'city': 'detroit', 'district': 'downtown'}
+    assert parse('http://example.com/?name=1') == {'name': '1'}
+    assert parse('http://example.com/?path=hell') == {'path': 'hell'}
+    assert parse('https://education.github.com/git-cheat-sheet-education.pdf') == {}
+    assert parse('https://www.youtube.com/watch?v=kusM4BtJ07g&t=1110s') == {'v': 'kusM4BtJ07g', 't':'1110s'}
+    assert parse('https://www.google.com/search?channel=fs&client=ubuntu&q=detroit') == {'channel': 'fs', 'client': 'ubuntu', 'q': 'detroit'}
+    assert parse('https://lms.ithillel.ua/') == {}
+    assert parse('http://example.com/bed.php?branch=bead') == {'branch': 'bead'}
+    assert parse('https://www.example.com/act.php?bee=argument') == {'bee': 'argument'}
+    assert parse('https://www.example.com/basin/art.php') == {}
 
 def parse_cookie(query: str) -> dict:
     return {}
